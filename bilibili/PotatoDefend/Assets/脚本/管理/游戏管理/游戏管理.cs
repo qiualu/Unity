578cb770ac37b78478a02cc381bdd5449e6f5eab
美哉 <494436488@qq.com>
2025-07-17 13:57:12 +0800

    v0.0.6 2025-07-17 13:57:10

--- bilibili/PotatoDefend/Assets/脚本/管理/游戏管理/游戏管理.cs
+++ bilibili/PotatoDefend/Assets/脚本/管理/游戏管理/游戏管理.cs
@@ -11,6 +11,21 @@ public class 游戏管理 : MonoBehaviour
     public 界面管理器 界面管理;
 
 
+    private static 游戏管理 _实例;
+
+    public static 游戏管理 实例
+    {
+        get
+        {
+            return _实例;
+        }
+    }
+
+    public 关卡 当前关卡;
+
+    public bool 是否重置玩家数据;//是否重置游戏
+
+
     // Start is called before the first frame update
     void Start()
     {
